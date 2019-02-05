4ecf7035b7c487342ffa55be663076cc8b40dcd7
JelleHU <31843404+AutoJelle@users.noreply.github.com>
2019-02-05 21:44:18 +0100

    Opdrachten gedaan

--- Opdracht1_2/main.cpp
+++ Opdracht1_2/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-using namespace std;
+// A:
+// x = 8
+// c = B
+// f = 8
+// u = 0
+// p = "Hoera! HIEP!"
+// station = "Driebergen-Zeist"
+// plaats = "Driebergen"
+// y = 3
+// z = 3
+// t = 3
+// s = 3.6
+// e = 1
+// b = 1
 
-int main()
-{
-	cout << "test\n";
-}
+// B:
+// De modulus, deze geeft het overgebleven aantal na het delen.
