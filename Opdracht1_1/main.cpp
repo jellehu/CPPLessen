0e22e84f012fef7d58bed07ce3df6eb7aa256427
JelleHU <31843404+AutoJelle@users.noreply.github.com>
2019-02-05 19:05:07 +0100

    Opdrachten gedaan

--- Opdracht1_1/main.cpp
+++ Opdracht1_1/main.cpp
@@ -3,5 +3,38 @@ using namespace std;
 
 int main()
 {
-	cout << "test\n";
+    // 1.
+    cout << "1. ASCII waardes:\n"
+         << "a: " << int('a') << "\n"
+         << "b: " << int('b') << "\n"
+         << "c: " << int('c') << "\n"
+         << "h: " << int('h') << "\n"
+         << "z: " << int('z') << "\n"
+         << "\n";
+
+    // 2.
+    cout << "2. ASCII waardes:\n"
+         << "A: " << int('A') << "\n"
+         << "B: " << int('B') << "\n"
+         << "C: " << int('C') << "\n"
+         << "H: " << int('H') << "\n"
+         << "Z: " << int('Z') << "\n"
+         << "\n";
+
+    // 3.
+    cout << "3.\n"
+         << "g - (a - A) = " << int('g') - (int('a') - int('A')) << "\n\n";
+
+    // 4.
+    cout << "4.\n"
+         << "8: " << int('8') << "\n"
+         << "0: " << int('0') << "\n"
+         << "\n";
+
+    // 5.
+    cout << "5.\n"
+         << "'4' - '0' = " << int('4') - int('0') << "\n\n";
+
+    // 6.
+    // Bij vraag 3 maak je er een hoofdletter van en bij vraag 6 krijg je de decimale waarde.
 }
